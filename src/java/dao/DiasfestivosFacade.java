9d1ec6aa48c601b76c4067054b2e660c336fc540
ND17613 <ND17613@EW620PC0AFCUU.dow.com>
2017-03-08 16:16:03 +0100

    Merge origin/master
    Conflicts:
    	src/java/servei/Tornservei.java

---
+++ src/java/dao/DiasfestivosFacade.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import domini.Diasfestivos;
+import java.io.Serializable;
+import javax.ejb.Stateless;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author ND17613
+ */
+@Named
+@SessionScoped
+public class DiasfestivosFacade extends AbstractFacade<Diasfestivos> implements Serializable{
+
+    @PersistenceContext(unitName = "GestorTurnosPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public DiasfestivosFacade() {
+        super(Diasfestivos.class);
+    }
+    
+}
