44628eb6feacc8f4afa9802f32da232c2a4deaa6
ND17615 <ND17615@EW620PF0J1Y8K.dow.com>
2017-03-20 09:35:12 +0100

    Merge origin/master
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml
    	web/WEB-INF/web.xml
    	web/index.xhtml

---
+++ web/resources/js/calendar_localization.js
@@ -0,0 +1,24 @@
+/* global PrimeFaces */
+
+PrimeFaces.locales['es'] = {
+        closeText: 'Cerrar',
+        prevText: 'Anterior',
+        nextText: 'Siguiente',
+        currentText: 'Año actual',
+        monthNames: ['Enero','Febrero','Marzo','Abril','Mayo','Junio',
+            'Julio','Agosto','Setiembre','Octubre','Noviembre','Diciembre'],
+        monthNamesShort: ['Ene','Feb','Mar','Abr','May','Jun',
+            'Jul','Ago','Set','Oct','Nov','Dic'],
+        dayNames: ['Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado'],
+        dayNamesShort: ['Dom','Lun','Mar','Mier','Jue','Vie','Sab'],
+        dayNamesMin: ['Dom','Lun','Mar','Mier','Jue','Vie','Sab'],
+        weekHeader: 'Semana',
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: '',
+        month: 'Mes',
+        week: 'Semana',
+        day: 'Dia',
+        allDayText : 'Todo el dia'
+    }
